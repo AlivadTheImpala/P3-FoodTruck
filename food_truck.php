c75f2dad9e77fe36de5bf1132e918be058b3957d
AlivadTheImpala <brandon.davila@seattlecolleges.edu>
2023-02-13 22:41:40 -0800

    no changes

--- food_truck.php
+++ food_truck.php
@@ -105,7 +105,7 @@ if (isset($_POST["submit"])) {
 
                 <?php } ?>
 
-                <!-- Add the extras section -->
+                <!-- Adds the extras section -->
                 <h3>Extras</h3>
                 <p>Cheese: $0.25</p>
 
